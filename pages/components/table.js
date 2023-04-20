0ad983ed065157d6c539a0330ba89836bc3dae3c
hftamayo <hftamayo@gmail.com>
2023-04-20 15:21:22 -0600

    first approach to table layout component

---
+++ pages/components/table.js
@@ -0,0 +1,12 @@
+export default function Table(){
+    return(
+        <table className="min-w-full table-auto">
+            <thead>
+                <tr>
+                    <span>Name</span>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    )
+}
