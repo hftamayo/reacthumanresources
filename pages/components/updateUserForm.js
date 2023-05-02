7570ae9b3cde3f57b8b6b5897cf767b83ccbb7db
hftamayo <hftamayo@gmail.com>
2023-05-02 17:39:27 -0600

    changes on update form

--- pages/components/updateUserForm.js
+++ pages/components/updateUserForm.js
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import { BiPlus } from "react-icons/bi";
+import { BiBrush } from "react-icons/bi";
 import Success from "./success";
 import Bug from "./bug";
 
@@ -110,13 +110,13 @@ export default function UpdateUserForm() {
         </div>
       </div>
       <button
-        className="flex justify-center text-md w-2/6 bg-green-500 
+        className="flex justify-center text-md w-2/6 bg-yellow-400 
       text-white px-4 py-2 border rounded-md hover:bg-gray-50 
       hover:border-green-500 hover:text-green-500"
       >
-        Add
+        Update
         <span className="px-1">
-          <BiPlus size={24}></BiPlus>
+          <BiBrush size={24}></BiBrush>
         </span>
       </button>
     </form>
