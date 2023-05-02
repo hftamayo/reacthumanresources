174a039b3aa9fba4b2df50dda83098a33b9c1737
hftamayo <hftamayo@gmail.com>
2023-05-02 17:23:18 -0600

    sentence for evalute showing Add or Update Form

--- pages/components/form.js
+++ pages/components/form.js
@@ -1,11 +1,12 @@
 import AddUserForm from "./addUserForm";
+import UpdateUserForm from "./updateUserForm";
 
 export default function Form() {
   const flag = true;
 
   return (
     <div className="container mx-auto py-5">
-      <AddUserForm></AddUserForm>
+      {flag ? <AddUserForm /> : <UpdateUserForm />}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,9 @@ import { useState } from "react";
 export default function Home() {
   const [visible, setVisible] = useState(false);
 
-  const handler = () =>{
-    setVisible(visible ? false : true);
-  } 
-
+  const handler = () => {
+    setVisible(!visible);
+  };
 
   return (
     <section>
@@ -27,7 +26,10 @@ export default function Home() {
 
         <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="left flex gap-3">
-            <button onClick={handler} className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800">
+            <button
+              onClick={handler}
+              className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
+            >
               Add Employee
               <span className="px-1">
                 <BiUserPlus size={23}></BiUserPlus>
@@ -37,9 +39,7 @@ export default function Home() {
         </div>
 
         {/* collapsable form */}
-        <div className="container mx-auto py-5">
-          {visible ? <Form></Form> : ""}
-        </div>
+        {visible ? <Form></Form> : ""}
 
         {/* table */}
         <div className="container mx-auto">
