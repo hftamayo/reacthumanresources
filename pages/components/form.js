38a27573bede968a264b4d90576fb2d56fa456be
hftamayo <hftamayo@gmail.com>
2023-05-02 17:20:48 -0600

    moving some css tags to form component

--- pages/components/form.js
+++ pages/components/form.js
@@ -1,7 +1,11 @@
 import AddUserForm from "./addUserForm";
 
-export default function Form(){
-  return(
-    <AddUserForm></AddUserForm>
+export default function Form() {
+  const flag = true;
+
+  return (
+    <div className="container mx-auto py-5">
+      <AddUserForm></AddUserForm>
+    </div>
   );
 }
