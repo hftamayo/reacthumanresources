24590cea36e031b577cb93852a27e2ec8585de83
hftamayo <hftamayo@gmail.com>
2023-05-08 13:10:09 -0600

    adding business logic for handshake connection

--- database/conn.js
+++ database/conn.js
@@ -1,4 +1,19 @@
-const MONGODB_USER: "node";
-const MONGODB_PASSWORD: "B3eG3es";
-const MONGODB_DATABASE: "humanresources";
-const MONGO_URI: "mongodb+srv://node:B3eG3es@nodecluster.hfnls.mongodb.net/humanresources";
+const MONGODB_USER = "node";
+const MONGODB_PASSWORD = "B3eG3es";
+const MONGODB_DATABASE = "humanresources";
+const MONGO_URI =
+  "mongodb+srv://node:B3eG3es@nodecluster.hfnls.mongodb.net/humanresources";
+
+import mongoose from "mongoose";
+
+const connectMongo = async () => {
+  try {
+    const { connection } = await mongoose.connect(MONGO_URI);
+
+    if (connection.readyState == 1) {
+      console.log("Database connected");
+    }
+  } catch (errors) {
+    return Promise.reject(errors);
+  }
+};
