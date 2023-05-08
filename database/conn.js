2303919eb0f4481955b5968e529e2c259b36fcfd
hftamayo <hftamayo@gmail.com>
2023-05-08 12:57:53 -0600

    mongodb connection string hardcoded

---
+++ database/conn.js
@@ -0,0 +1,4 @@
+const MONGODB_USER: "node";
+const MONGODB_PASSWORD: "B3eG3es";
+const MONGODB_DATABASE: "humanresources";
+const MONGO_URI: "mongodb+srv://node:B3eG3es@nodecluster.hfnls.mongodb.net/humanresources";
