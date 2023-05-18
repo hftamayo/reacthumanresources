fe07984cc1ca1f4a0cefb6a95cc44d55552e36dc
hftamayo <hftamayo@gmail.com>
2023-05-18 14:37:06 -0600

    managing connection with a diff approach

--- database/conn.js
+++ database/conn.js
@@ -1,14 +1,8 @@
-const MONGODB_USER = "node";
-const MONGODB_PASSWORD = "B3eG3es";
-const MONGODB_DATABASE = "humanresources";
-const MONGO_URI =
-  "mongodb+srv://node:B3eG3es@nodecluster.hfnls.mongodb.net/humanresources";
-
 import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    const { connection } = await mongoose.connect(MONGO_URI);
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
 
     if (connection.readyState == 1) {
       console.log("Database connected");
---
+++ database/controller.js
@@ -0,0 +1,9 @@
+
+export async function getUsers(req, res){
+    try{
+        res.status(200).json({ user: "Get Request"})
+
+    }catch(error){
+        res.status(404).json({ error: "Error while fetching data"})
+    }
+}
