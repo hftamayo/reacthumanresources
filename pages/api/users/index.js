3232d99af7bc96d2a0b081c53535e1afbab099a6
hftamayo <hftamayo@gmail.com>
2023-05-18 14:22:07 -0600

    polishing component for request handling

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,21 +1,28 @@
 import connectMongo from "../../../database/conn";
 
 export default async function handler(req, res) {
-  connectMongo()
+  connectMongo().catch(() =>
+    res.status(405).json({ error: "Error in the Connection" })
+  );
 
-  const {method} = req
+  const { method } = req;
 
-  switch(method){
-    case 'GET':
-        res.status(200).json({ method, name: 'GET Request'});
-    case 'POST':
-        res.status(200).json({ method, name: 'POST Request'});
-    case 'PUT':
-        res.status(200).json({ method, name: 'PUT Request'});                    
-    case 'DELETE':
-        res.status(200).json({ method, name: 'DELETE Request'}); 
+  switch (method) {
+    case "GET":
+      res.status(200).json({ method, name: "GET Request" });
+      break;
+    case "POST":
+      res.status(200).json({ method, name: "POST Request" });
+      break;
+    case "PUT":
+      res.status(200).json({ method, name: "PUT Request" });
+      break;
+    case "DELETE":
+      res.status(200).json({ method, name: "DELETE Request" });
+      break;
     default:
-        res.setHeader('Allow', ['GET','POST','PUT','DELETE']);
-        res.status(405).end(`Method${method} Not Allowed` )                           
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method${method} Not Allowed`);
+      break;
   }
 }
