8b07436999769bae98e1733cf6208a5804baaf52
hftamayo <hftamayo@gmail.com>
2023-05-22 15:25:11 -0600

    get and post methods first version

--- database/controller.js
+++ database/controller.js
@@ -1,9 +1,29 @@
+import Users from "../model/user";
 
-export async function getUsers(req, res){
+//get endpoint: http://localhost:3000/api/users
+
+export async function getUsers(req, res) {
+  try {
+    const users = Users.find({});
+
+    if (!users) return res.status(404).json({ error: "Data not found" });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ error: "Error while fetching data" });
+  }
+}
+
+//post endpoint: http://localhost:3000/api/users
+export async function postUser(req, res){
     try{
-        res.status(200).json({ user: "Get Request"})
+        const formData = req.body;
+        if(!formData) return res.status(404).json({ error: "Form data not provided"});
+        Users.create( formData, function(err, data){
+            return res.status(200).json(data);
+        });
 
     }catch(error){
-        res.status(404).json({ error: "Error while fetching data"})
+        return res.status(404).json({ error })
+
     }
 }
