d8829a733fc617be81a26ce00193dcad0a74ce92
hftamayo <hftamayo@gmail.com>
2023-05-22 15:56:01 -0600

    bug inserting a record

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,5 @@
 import connectMongo from "../../../database/conn";
-import { getUsers } from "../../../database/controller";
+import { getUsers, postUser } from "../../../database/controller";
 
 export default async function handler(req, res) {
   connectMongo().catch(() =>
@@ -10,10 +10,10 @@ export default async function handler(req, res) {
 
   switch (method) {
     case "GET":
-      getUsers(req, res)
+      getUsers(req, res);
       break;
     case "POST":
-      res.status(200).json({ method, name: "POST Request" });
+      postUser(req, res);
       break;
     case "PUT":
       res.status(200).json({ method, name: "PUT Request" });
