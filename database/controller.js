f3ca1e07a16e6c9fefcff46960f0d8a0a51080b3
hftamayo <hftamayo@gmail.com>
2023-05-29 15:54:47 -0600

    put method sample

--- database/controller.js
+++ database/controller.js
@@ -14,16 +14,31 @@ export async function getUsers(req, res) {
 }
 
 //post endpoint: http://localhost:3000/api/users
-export async function postUser(req, res){
-    try{
-        const formData = req.body;
-        if(!formData) return res.status(404).json({ error: "Form data not provided"});
-        Users.create( formData, function(err, data){
-            return res.status(200).json(data);
-        });
+export async function postUser(req, res) {
+  try {
+    const formData = req.body;
+    if (!formData)
+      return res.status(404).json({ error: "Form data not provided" });
+    Users.create(formData, function (err, data) {
+      return res.status(200).json(data);
+    });
+  } catch (error) {
+    return res.status(404).json({ error });
+  }
+}
 
-    }catch(error){
-        return res.status(404).json({ error })
+//put method: http://localhost:3000/api/users/1
+export async function putUSer(req, res) {
+  try {
+    const { userId } = req.query;
+    const formData = req.body;
 
+    if (userId && formData) {
+      await Users.findByIdAndUpdate(userId, formData);
+      res.status(200).json(formData);
     }
+    res.status(404).json({ error: "User not selected!" });
+  } catch (error) {
+    res.status(404).json({ error: "Error while updating the data" });
+  }
 }
