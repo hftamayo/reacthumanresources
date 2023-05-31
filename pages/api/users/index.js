86cb59943a17f5e4e43166347d9cb53a51fb0e39
hftamayo <hftamayo@gmail.com>
2023-05-31 15:40:45 -0600

    adding return to the test endpoints

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -11,21 +11,21 @@ export default async function handler(req, res) {
   switch (method) {
     case "GET":
       // getUsers(req, res);
-      res.status(200).json({ method, name: "GET Request" });      
+      return res.status(200).json({ method, name: "GET Request" });
       break;
     case "POST":
-      res.status(200).json({ method, name: "POST Request" });
+      return res.status(200).json({ method, name: "POST Request" });
       // postUser(req, res);
       break;
     case "PUT":
-      res.status(200).json({ method, name: "PUT Request" });
+      return res.status(200).json({ method, name: "PUT Request" });
       break;
     case "DELETE":
-      res.status(200).json({ method, name: "DELETE Request" });
+      return res.status(200).json({ method, name: "DELETE Request" });
       break;
     default:
       res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
-      res.status(405).end(`Method${method} Not Allowed`);
+      return res.status(405).end(`Method${method} Not Allowed`);
       break;
   }
 }
